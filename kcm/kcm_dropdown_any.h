419eb7652e345c650276b2e383290a40cbe528cd
Robinson Giraldo <rgn159@gmail.com>
2026-05-25 21:18:44 -0500

    feat(kcm): extend SlotData with screenTarget, declare screenNames property

--- kcm/kcm_dropdown_any.h
+++ kcm/kcm_dropdown_any.h
@@ -16,6 +16,7 @@ class DropdownAnyKCM : public KQuickConfigModule
     Q_PROPERTY(QStringList activeWindows READ activeWindows NOTIFY activeWindowsChanged)
     Q_PROPERTY(QVariantList slots        READ slots         NOTIFY slotsChanged)
     Q_PROPERTY(bool debugMode READ debugMode WRITE setDebugMode NOTIFY debugModeChanged)
+    Q_PROPERTY(QStringList screenNames   READ screenNames   NOTIFY screenNamesChanged)
 
 public:
     DropdownAnyKCM(QObject *parent, const KPluginMetaData &data);
@@ -23,12 +24,14 @@ public:
     QStringList activeWindows() const;
     QVariantList slots() const;
     bool debugMode() const;
+    QStringList screenNames() const;
 
     Q_INVOKABLE void setSlot(int idx,
                              const QString &windowClass,
                              const QString &shortcut,
                              int widthPct,
-                             int heightPct);
+                             int heightPct,
+                             int screenTarget);
     Q_INVOKABLE void addSlot();
     Q_INVOKABLE void addSlotWithClass(const QString &windowClass);
     Q_INVOKABLE void removeSlot(int idx);
@@ -43,9 +46,11 @@ Q_SIGNALS:
     void activeWindowsChanged();
     void slotsChanged();
     void debugModeChanged();
+    void screenNamesChanged();
 
 private Q_SLOTS:
     void finalizeWindowCollection();
+    void rebuildScreenNames();
 
 private:
     struct SlotData {
@@ -53,10 +58,12 @@ private:
         QString shortcut;
         int widthPercent  = 100;
         int heightPercent = 50;
+        int screenTarget  = 0;
     };
 
     QList<SlotData> m_slots;
     QStringList     m_activeWindows;
+    QStringList     m_screenNames;
     WindowSink     *m_winSink = nullptr;
     bool            m_debugMode = false;
 
